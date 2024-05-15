7a433efe937fd8fb70810146e4226b9e4482d3b8
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2024-05-15 12:44:54 +0200

    WIP

--- examples/todo_list_jwt/domain/entries/entries.py
+++ examples/todo_list_jwt/domain/entries/entries.py
@@ -9,12 +9,12 @@ class ListState(State):
 
 class EntriesViewModel:
     def __init__(self) -> None:
-        self.state = []
+        self.state = ListState()
 
     def add(self, entry: str) -> ListState:
         self.state.entries.append({"text": entry, "id": len(self._list)})
-        return ListState()
+        return self.state
 
     def remove(self, entry_id: int) -> ListState:
         self.state.entries = [x for x in self._list if x.id != entry_id]
-        return ListState()
+        return self.state
